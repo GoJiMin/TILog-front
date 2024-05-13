5c3ab33105dfaf04001a0a24b37adf46eccdbdd1
GoJiMin <gojimin3095@gmail.com>
2024-05-13 21:27:13 +0900

    feat: 포스팅 기능 구현 - 클라이언트
    - 클라이언트 단에서 submit 이벤트가 발생하면 /api/posts 엔드포인트를 이용해 formData를 생성해 전달합니다.
    - 포스팅 과정의 loading과 error state에 따른 UI를 구현했습니다.
    - textarea를 ref를 이용해 컨트롤합니다. 리렌더링 이슈

--- src/app/components/NewPost.tsx
+++ src/app/components/NewPost.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { ChangeEvent, DragEvent, useState } from "react";
+import { ChangeEvent, DragEvent, FormEvent, useRef, useState } from "react";
 import { AuthUser } from "../model/user";
 import Avatar from "./ui/Avatar";
 import Button from "./ui/Button";
 import { FilesIcon } from "./ui/icons";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import PulseSpinner from "./ui/PulseSpinner";
 
 type Props = {
   user: AuthUser;
@@ -15,13 +17,18 @@ export default function NewPost({ user: { profileimage, userid } }: Props) {
   const [dragging, setDragging] = useState(false);
   const [file, setFile] = useState<File>();
 
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>();
+  const textRef = useRef<HTMLTextAreaElement>(null);
+
+  const router = useRouter();
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
 
     const files = e.target?.files;
     if (files && files[0]) {
       setFile(files[0]);
-      console.log(files[0]);
     }
   };
 
@@ -46,17 +53,53 @@ export default function NewPost({ user: { profileimage, userid } }: Props) {
     const files = e.dataTransfer?.files;
     if (files && files[0]) {
       setFile(files[0]);
-      console.log(files[0]);
     }
   };
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!file) return;
+
+    setError("");
+    setLoading(true);
+    const formData = new FormData();
+
+    formData.append("file", file);
+    formData.append("text", textRef.current?.value ?? "");
+
+    fetch("/api/posts/", { method: "POST", body: formData }) //
+      .then((res) => {
+        if (!res.ok) {
+          setError(`${res.status} ${res.statusText}`);
+          return;
+        }
+
+        // router.push("/");
+      })
+      .catch((err) => setError(err.toString()))
+      .finally(() => setLoading(false));
+  };
+
+  console.log(error);
+
   return (
-    <section className="w-full max-w-xl mx-auto mt-10 flex flex-col">
+    <section className="relative w-full max-w-xl mx-auto mt-[40px] flex flex-col">
+      {loading && <PulseSpinner />}
+      {loading && <div className="fixed inset-0 z-10 bg-black/20" />}
+      {error && (
+        <p className="w-full bg-red-200 text-red-600 text-center font-semibold p-4 mb-4 rounded-md">
+          {error}
+        </p>
+      )}
       <article className="flex gap-[10px] mb-[10px]">
         <Avatar image={profileimage} size="small" />
         <p className="text-lg font-semibold">{userid}</p>
       </article>
-      <form className="w-full flex flex-col gap-[10px] mt-2">
+      <form
+        className="w-full flex flex-col gap-[10px] mt-2"
+        onSubmit={handleSubmit}
+      >
         <input
           id="input-upload"
           className="hidden"
@@ -105,6 +148,7 @@ export default function NewPost({ user: { profileimage, userid } }: Props) {
           required
           rows={10}
           placeholder="오늘의 공유를 시작해보세요!"
+          ref={textRef}
         />
         <Button text="게시" onClick={() => {}} />
       </form>
